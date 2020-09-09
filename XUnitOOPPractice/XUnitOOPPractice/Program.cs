16d2653aeef5b63efbc16715573f3715b679260d
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-09-09 15:50:54 -0600

    Initial commit.

---
+++ XUnitOOPPractice/XUnitOOPPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XUnitOOPPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
